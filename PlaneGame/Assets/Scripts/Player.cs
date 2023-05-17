9d6525d46b00c3822c9a85be7865308132c1bc3b
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-05-17 19:51:03 +0900

    Update Player.cs

--- PlaneGame/Assets/Scripts/Player.cs
+++ PlaneGame/Assets/Scripts/Player.cs
@@ -16,7 +16,10 @@ public class Player : MonoBehaviour
     float cur_delay = 0;
     float bullet_speed = 6;
 
-    bool hit_leftbox=false;
+    bool hit_leftbox =false;
+    bool hit_rightbox = false;
+    bool hit_topbox = false;
+    bool hit_bottombox = false;
 
 
     void Start()
@@ -30,12 +33,21 @@ public class Player : MonoBehaviour
         cur_delay = cur_delay + Time.deltaTime; //0.001
 
         inputVec.x = Input.GetAxisRaw("Horizontal");
+        
+        if ((hit_leftbox&&inputVec.x==-1) || (hit_rightbox && inputVec.x == 1))
+        {
+            inputVec.x = 0;
+        }
+
         inputVec.y = Input.GetAxisRaw("Vertical");
 
-        Fire();
-        
+        if ((hit_topbox && inputVec.y == 1) || (hit_bottombox && inputVec.y == -1))
+        {
+            inputVec.y = 0;
+        }
 
-        
+        Fire();
+             
     }
 
     private void FixedUpdate()
@@ -70,20 +82,75 @@ public class Player : MonoBehaviour
             {
                 hit_leftbox = true;
             }
+            else if(collision.transform.name == "RightBoundary")
+            {
+                hit_rightbox = true;
+            }
+            else if (collision.transform.name == "TopBoundary")
+            {
+                hit_topbox = true;
+            }
+            else if (collision.transform.name == "BottomBoundary")
+            {
+                hit_bottombox = true;
+            }
+
         }
         
+        //if (collision.transform.tag == "Boundary")
+        //{
+        //    switch (collision.transform.name)
+        //    {
+        //        case "LeftBoundary":
+        //            hit_leftbox = true;
+        //            break;
+
+        //        case "RightBoundary":
+        //            hit_rightbox = true;
+        //            break;
+
+        //        case "TopBoundary":
+        //            hit_topbox = true;
+        //            break;
+
+        //        case "BottomBoundary":
+        //            hit_bottombox = true;
+        //            break;
+
+        //    }
+
+
+        //}
+
+
+
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.tag == "Boundary")
         {
-            if (collision.transform.name == "LeftBoundary")
+            switch (collision.transform.name)
             {
-                hit_leftbox = false;
+                case "LeftBoundary":
+                    hit_leftbox = false;
+                    break;
+
+                case "RightBoundary":
+                    hit_rightbox = false;
+                    break;
+
+                case "TopBoundary":
+                    hit_topbox = false;
+                    break;
+
+                case "BottomBoundary":
+                    hit_bottombox = false;
+                    break;
+
             }
         }
-
     }
 
 
