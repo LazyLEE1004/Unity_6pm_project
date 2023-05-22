266deaf83fe81b8023ef1c73fb94edbb7338966f
shlee <shlee6630@gmail.com>
2023-05-22 18:30:53 +0900

    add_background

--- PlaneGameStage2/Assets/Scripts/Enemy.cs
+++ PlaneGameStage2/Assets/Scripts/Enemy.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float cur_timer=0;
+    float destory_timer=5;
+
+    float hp = 3;
+    
     void Start()
     {
         
@@ -13,6 +18,35 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        cur_timer += Time.deltaTime;
+        //cur_timer = cur_timer + Time.deltaTime;
+
+        if(cur_timer> destory_timer)
+        {
+            Destroy(gameObject);
+            cur_timer= 0;
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            hp = hp - 1;
+            
+            Destroy(collision.gameObject);
+
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
         
     }
 
